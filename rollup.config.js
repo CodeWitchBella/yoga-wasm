2ec8a9f8d794265dcd5218ab64188708a0e8d0f2
Isabella Skořepová <isabella@skorepova.info>
2019-08-29 20:24:43 +0200

    Simplify build

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
 import commonjs from "rollup-plugin-commonjs";
 import builtins from "rollup-plugin-node-builtins";
 
-export default {
+const base = {
   input: "src/index.js",
   context: "window",
   plugins: [commonjs(), builtins()],
-};
+}
+
+export default [{
+  ...base,
+  output: {
+    format: "es",
+    file: "dist/Yoga.es.js",
+  },
+}, {
+  ...base,
+  output: {
+    format: "cjs",
+    file: "dist/Yoga.cjs.js",
+  }
+}];
